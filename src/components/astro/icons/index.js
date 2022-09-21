cfbfa9a9827a14f53886162538e02ae66a7af6e8
TheSpaceAce <90187370+The0n1y5pace4ce@users.noreply.github.com>
2022-09-21 13:04:25 +1000

    Delete src directory

--- src/components/astro/icons/index.js
+++
@@ -1,6 +0,0 @@
-import IconTheme from "./IconTheme.astro";
-import IconGithub from "./IconGithub.astro";
-import IconArrowRight from "./IconArrowRight.astro";
-import IconMenu from "./IconMenu.astro";
-
-export { IconTheme, IconGithub, IconArrowRight, IconMenu };
